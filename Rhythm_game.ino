156a4e4af07df9d523bd29cf753ea2abc6a3d4f3
Oyaki <oc.yaki122@gmail.com>
2019-01-12 23:37:21 +0900

    軽量化のため関数をなくした

--- Rhythm_game.ino
+++ Rhythm_game.ino
@@ -2,50 +2,33 @@
 #define buzzer 2
 #define led 3
 
-short tempoDecide(char notesNum) {
-    return 60 / (2 * 25 * notesNum + random(-10, 11)) * 1000;
-}
-
-void rhythmGenerator(const short* rhythm, const char notesNum) {
-    for (char count = 0; count < notesNum; count++) {
-        rhythm[count] = random(0, 8);
-    }
-}
-
-void modelShow(const short* rhythm, const char notesNum, const short tempo) {
-    for (char count = 0; count < notesNum; count++) {
-        tone(buzzer, 1000, tempo * rhythm[count] / 2);
-        delay(tempo * rhythm[count]);
-    }
-}
-
 void setup() {
-    pinMode(button, INPUT_PULLUP);
-    pinMode(buzzer, OUTPUT);
-    pinMode(led, OUTPUT);
+    DDRB = 0b01;
+    PORTB = 0b101;
 
-    digitalWrite(led, HIGH);
-    tone(buzzer, 1000, 1000);
+    // tone(buzzer, 1000, 1000);
     delay(1000);
-    digitalWrite(led, LOW);
+    PORTB &= ~0b100;
     delay(1000);
-}
+    short rhythm[5], play[5], tempo, result;
 
-void loop() {
-    short rhythm[5];
-    short play[5];
-    short tempo;
-    bool result;
+    for (char notesNum = 4; notesNum < 9; notesNum++) {
+        tempo = 6 / (5 * notesNum);
+        for (char count = 0; count < notesNum; count++) {
+            rhythm[count] = random(0, 8);
+        }
+        for (char count = 0; count < notesNum; count++) {
+            digitalWrite(2, HIGH);
+            delay(tempo * rhythm[count] / 2);
+            digitalWrite(2, LOW);
+            delay(tempo * rhythm[count] / 2);
+        }
 
-    for (char notesNum = 4; i < 9; i++) {
-        tempo = tempoDecide(notesNum);
-        rhythmGenerator(&rhythm, notesNum);
-        modelShow(&rhythm, notesNum, tempo);
-        tone(buzzer, 500, 500);
+        // tone(buzzer, 500, 500);
         delay(500);
-        playInput(&play, tempo);
-        result = judge(&rhythm, &play);
-        showResult(result);
+
         delay(1000);
     }
 }
+
+void loop() {}
